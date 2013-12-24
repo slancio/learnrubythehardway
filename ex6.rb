9b3e1f09d9c94e26e20257e16f2f4b6a0fbdb945
Eric Savadian <eric.j.savadian@gmail.com>
2013-12-23 23:20:02 -0500

    Ex6: rest of extra credit

--- ex6.rb
+++ ex6.rb
@@ -1,4 +1,5 @@
 # Defines a variable x containing a string.  #{10} in the string evaluates to the number 10.
+# Extra Credit 2: Here >
 x = "There are #{10} types of people."
 
 # Defines a variable binary containing string "binary"
@@ -8,6 +9,7 @@ binary = "binary"
 do_not = "don't"
 
 # Defines a variable y containing a string which also evaluates the variables binary and do_not.  Joins 3 separate strings
+# Extra Credit 2: Here, twice >
 y = "Those who know #{binary} and those who #{do_not}."
 
 # prints x
@@ -16,14 +18,17 @@ puts x
 puts y
 
 # prints a string containing the string variable x
+# Extra Credit 2: Here >
 puts "I said: #{x}"
 # prints a string containing the string variable y
+# Extra Credit 2: Here >
 puts "I also said: '#{y}.'"
 
 # defines variable hilarious containing boolean false
 hilarious = false
 
 # defines variable joke_evaluation as a string containing variable hilarious converted to a string
+# Extra Credit 2: Here >
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
 # prints joke_evaluation
@@ -36,4 +41,5 @@ w = "This is the left side off..."
 e = "a string with a right side."
 
 # prints a concatenation of strings in variables w and e
+# Extra Credit 4: Strings can be added together to make longer strings.  Concatenation.
 puts w + e
