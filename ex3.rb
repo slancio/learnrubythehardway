a2de58a6743199277f82a80e0e5510ad025c5988
Eric Savadian <eric.j.savadian@gmail.com>
2013-12-23 19:54:49 -0500

    ex3: final extra credit

--- ex3.rb
+++ ex3.rb
@@ -1,37 +1,23 @@
-# prints "I will now count my chickens:"
 puts "I will now count my chickens:"
 
-# prints "Hens", then on a newline calculates 25 + (30/6) due to order of operations
-puts "Hens", 25 + 30 / 6
-# prints "Roosters", then on a newline calculates 100 - ((25 * 3) % 4) due to order of operations
-puts "Roosters", 100 - 25 * 3 % 4
+puts "Hens", 25.0 + 30.0 / 6.0
+puts "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
-# prints "Now I will count the eggs:"
 puts "Now I will count the eggs:"
 
-# calculates and prints 3 + 2 + 1 - 5 + (4 % 2) - (1 / 4) + 6.  All integers so no fractions, so 1 / 4 is 0.
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
-# prints "Is it true that 3 + 2 < 5 - 7?"
-puts "Is it true that 3 + 2 < 5 - 7?"
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
 
-# calculates and prints 5 < -2
-puts 3 + 2 < 5 - 7
+puts 3.0 + 2.0 < 5.0 - 7.0
 
-# prints "What is 3 + 2?", then on a newline calculates 3 + 2
-puts "What is 3 + 2?", 3 + 2
-# prints "What is 5 - 7?", then on a newline calculates 5 - 7
-puts "What is 5 - 7?", 5 - 7
+puts "What is 3.0 + 2.0?", 3.0 + 2.0
+puts "What is 5.0 - 7.0?", 5.0 - 7.0
 
-# prints "Oh, that's why it's false."
 puts "Oh, that's why it's false."
 
-# prints "How about some more."
 puts "How about some more."
 
-# prints "Is it greater?", then on a newline evaluates 5 > -2 and returns true
-puts "Is it greater?", 5 > -2
-# prints "Is it greater or equal?", then on a newline evaluates 5 >= -2 and returns true
-puts "Is it greater or equal?", 5 >= -2
-# prints "Is it less than or equal?", then on a newline evaluates 5 <= -2 and returns true
-puts "Is it less than or equal?", 5 <= -2
+puts "Is it greater?", 5.0 > -2.0
+puts "Is it greater or equal?", 5.0 >= -2.0
+puts "Is it less than or equal?", 5.0 <= -2.0
