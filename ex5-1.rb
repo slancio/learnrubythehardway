5d1019817cf1da286f05394ea2982fdc37f9a4f7
Eric Savadian <eric.j.savadian@gmail.com>
2013-12-23 20:56:18 -0500

    ex5: extra credit

---
+++ ex5-1.rb
@@ -0,0 +1,11 @@
+phone = 'iPhone'
+cores = 2
+brand = 'Lenovo'
+model = 'Thinkpad x100e'
+item = 'wallet'
+money = 0
+
+puts "I am typing this on a %s %s." % [brand, model]
+puts "This is the second version with %d cores." % cores
+puts "Next to me are my %s and my wallet." % phone
+puts "The amount of money inside my wallet is: %d" % money
