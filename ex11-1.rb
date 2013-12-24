e9126216ac07cf159e7faf4b1b537be203d6c451
Eric Savadian <eric.j.savadian@gmail.com>
2013-12-24 01:25:56 -0500

    Exercise 11 and extra credit

---
+++ ex11-1.rb
@@ -0,0 +1,9 @@
+puts "Stop. Who approacheth the Bridge of Death must answer me these questions three, ere the other side he see."
+print "What... is your name? "
+name = gets.chomp()
+print "What... is your quest? "
+quest = gets.chomp()
+print "What... is your favorite color? "
+color = gets.chomp()
+puts "Right. Off you go."
+puts "#{quest}?!?!?  #{color}?!?!?  That #{name} got off easy."
