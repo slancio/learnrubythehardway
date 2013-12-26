01d77dfeeff6f434846ad5d13fe78df47408744e
Eric Savadian <eric.j.savadian@gmail.com>
2013-12-25 21:10:42 -0500

    Ex15: rewrote w/ better practice

--- ex15.rb
+++ ex15.rb
@@ -1,15 +1,15 @@
 filename = ARGV.first
 
 prompt = "> "
-txt = File.open(filename)
 
 puts "Here's your file: #{filename}"
-puts txt.read()
+File.open(filename) do |txt|
+  puts txt.read()
+end
 
 puts "I'll also ask you to type it again:"
 print prompt
 file_again = STDIN.gets.chomp()
-
-txt_again = File.open(file_again)
-
-puts txt_again.read()
+File.open(file_again) do |txt_again|
+  puts txt_again.read()
+end
