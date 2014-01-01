f7f27cd154eac173fe5d9eea99e2730b2f0bac25
Eric Savadian <eric.j.savadian@gmail.com>
2014-01-01 01:12:08 -0500

    Ex35: Extra credit

--- ex35.rb
+++ ex35.rb
@@ -6,13 +6,8 @@ def gold_room()
 	puts "This room is full of gold.  How much do you take?"
 
 	prompt; next_move = gets.chomp
-	if next_move.include? "0" or next_move.include? "1"
-		how_much = next_move.to_i()
-	else
-		dead("Man, learn to type a number.")
-	end
 
-	if how_much < 50
+	if next_move.to_i() < 50
 		puts "Nice, you're not greedy, you win!"
 		Process.exit(0)
 	else
