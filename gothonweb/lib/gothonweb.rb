4c4c34b49f463062c83565b6926488d5502bbf8a
Eric Savadian <eric.j.savadian@gmail.com>
2014-01-05 00:25:44 -0500

    Exercise 52

--- gothonweb/lib/gothonweb.rb
+++ gothonweb/lib/gothonweb.rb
@@ -1,8 +1,32 @@
 require_relative "gothonweb/version"
+require_relative "map"
 require "sinatra"
 require "erb"
 
+use Rack::Session::Pool
+
 get '/' do
-	greeting = "Hello, World!"
-	erb :index, :locals => {:greeting => greeting}
+	# this is used to "setup" the session with starting values
+	p START
+	session[:room] = START
+	redirect("/game")
 end
+
+get '/game' do
+	if session[:room]
+		erb :show_room, :locals => {:room => session[:room]}
+	else
+		# why is this here? do you need it?
+		erb :you_died
+	end
+end
+
+post '/game' do
+	action = "#{params[:action] || nil}"
+	# there is a bug here, can you fix it?
+	if session[:room]
+		session[:room] = session[:room].go(params[:action])
+	end
+	redirect("/game")
+end
+
